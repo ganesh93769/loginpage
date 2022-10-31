be50a98593a0cbf44b84a2fdbf5fcd0df83b60f3
ganesh93769 <ganeshreddy256@gmail.com>
2022-10-31 15:45:39 +0530

    Login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Login } from './login';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'loginpage';
+  model = new Login('', '');
+  submitted = false;
+  onSubmit() {this.submitted = true;}
+  newLogin(){
+    this.model = new Login('', '');
+  }
 }
